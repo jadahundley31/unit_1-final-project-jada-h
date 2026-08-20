52e0dcd6870bdda80b2962d70edc75ba27fc6613
Jada Hundley <jadahundley@gmail.com>
2026-08-20 17:15:53 -0500

    was able to display all 15 questions only. Fetch is working.

--- LaunchCode2026/launchcode/beyond-the-chapters/src/components/Quiz.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/Quiz.jsx
@@ -1,14 +1,36 @@
-import personalityQuestions from "/mock-data/personalityQuestions";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [questions, setQuestions] = useState([]);
 
-}
-const [questions, setQuestions] = useState(null);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [scores, setScores] = useState(0);
+  useEffect(() => {
+    fetch("/mock-data/personalityQuestions.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to load file");
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        setQuestions(jsonData);
+      })
+      .catch((error) => {
+        console.log("Error loading data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {questions.map((question) => {
+        return (
+          <div>
+            <li key={question.id}>{question.question}</li>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Quiz;
