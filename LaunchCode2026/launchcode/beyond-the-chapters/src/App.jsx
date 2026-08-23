0d82f38ff3920148e601022acbb46c3ff2c7a9d7
Jada Hundley <jadahundley@gmail.com>
2026-08-23 14:09:58 -0500

    imported routes and route for navigation.

--- LaunchCode2026/launchcode/beyond-the-chapters/src/App.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/App.jsx
@@ -1,13 +1,17 @@
-import Quiz from './components/Quiz'
+import Quiz from './components/pages/personality-quiz/Quiz'
+import { Routes, Route } from "react-router"
 import './App.css'
+import Results from './components/pages/personality-quiz/Results'
 
 function App() {
  
-
+//TODO: Add a Homepage to Route
   return (
-    <>
-    <Quiz/>
-    </>
+    <Routes>
+      <Route path='/quiz' element={<Quiz/>} />
+      <Route path='/results' element={<Results/>} />
+      <Route path='*' element={<Navigate to="/"/>} />
+    </Routes>
   )
 }
 
--- LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Quiz.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Quiz.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react";
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
