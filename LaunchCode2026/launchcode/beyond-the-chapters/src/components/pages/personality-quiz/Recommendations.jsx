7ee93f12d3291ecae2f0a52198bd5ff5f3fbf4df
Jada Hundley <jadahundley@gmail.com>
2026-08-24 19:27:45 -0500

    created a Recommendations.jsx fileto fetch and display recommended books for personality result.

---
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Recommendations.jsx
@@ -0,0 +1,7 @@
+
+
+const Recommendations = ({ personality }) => {
+
+}
+
+export default Recommendations;
--- LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Results.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Results.jsx
@@ -1,14 +1,15 @@
 import { useLocation } from "react-router";
+import Recommendations from "./Recommendations";
 
 const Results = () => {
     const location = useLocation();
-    console.log(location);
     return (
         <div>
             <h1> Your Personality Result: </h1>
             <p>{location.state.result}</p>
         </div>
     );
+    <Recommendations personality={result}/>
 }
 
 export default Results;
