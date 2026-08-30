69bca5a2a3262d6551c195ddb65e76a3f2f46722
Jada Hundley <jadahundley@gmail.com>
2026-08-30 12:10:03 -0500

    added unordered list tag in the navigation bar inside Header.jsx

--- LaunchCode2026/launchcode/beyond-the-chapters/src/components/layout/Header.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/layout/Header.jsx
@@ -1,26 +1,32 @@
-import './layout.css';
+import "./layout.css";
 import { Link, useLocation } from "react-router";
 
 const Header = () => {
-    const location = useLocation();
-    return (
-        <header>
-            <div className='logo-container'>
-                {<img id ="logo" src="../images/logo.png" alt="beyond-the-chapters-logo"/>}
-                <strong>Beyond the Chapters</strong>
-            </div>
-            <nav>
-                {location.pathname !== "/home" && (
-                    <Link to="/home">Home</Link>
-                )}
-                {location.pathname !== "/quiz" && (
-                    <Link to="/quiz">Start Quiz</Link>
-                )}
-                <Link to="/user">My Books</Link>
-                <Link to="/about">About</Link>
-            </nav>
-        </header>
-    )
-}
+  const location = useLocation();
+  return (
+    <header>
+      <div className="logo-container">
+        {
+          <img
+            id="logo"
+            src="../images/logo.png"
+            alt="beyond-the-chapters-logo"
+          />
+        }
+        <strong>Beyond the Chapters</strong>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            {location.pathname !== "/home" && <Link to="/home">Home</Link>}
+          </li>
+          <li>{location.pathname !== "/quiz" && <Link to="/quiz">Start Quiz</Link>}</li>
+          <li><Link to="/user">My Books</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
