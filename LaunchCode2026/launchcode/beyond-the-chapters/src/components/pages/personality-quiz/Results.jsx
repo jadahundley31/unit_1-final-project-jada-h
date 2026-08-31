78acbb4d84ece1e4831c8c0bd80900bfa4b23c7a
Jada Hundley <jadahundley@gmail.com>
2026-08-30 21:48:24 -0500

    created a guard for whenever a user tries to go into the results page without taking the quiz first. A message to take the quiz appears with a button.

--- LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Results.jsx
+++ LaunchCode2026/launchcode/beyond-the-chapters/src/components/pages/personality-quiz/Results.jsx
@@ -1,13 +1,26 @@
 import { useLocation, useNavigate } from "react-router";
 import Recommendations from "./Recommendations";
+import "./results-recommendations.css";
 
 const Results = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
+    if(!location.state) {
+        return (
+            <div>
+                <p>No results found. Please take the quiz first.</p>
+                <button onClick={() => navigate("/quiz")}>
+                    Take Quiz
+                </button>
+            </div>
+        );    
+    }
+
     const handleRetakeQuiz = () => {
         navigate("/quiz")
     }
+
     return (
         <main className="results-page">
             <section className="results-header">
