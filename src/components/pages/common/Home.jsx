193e9fa1a07f8bc08f18ae65606460e5c08d71be
Jada Hundley <jadahundley@gmail.com>
2026-09-01 21:03:13 -0500

    Prepare project for GitHub Pages deployment

---
+++ src/components/pages/common/Home.jsx
@@ -0,0 +1,63 @@
+import { useNavigate } from "react-router";
+import "./home.css";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleFindRead = () => {
+    navigate("/quiz");
+  };
+
+  return (
+    <main className="home-section">
+      <section className="home-text">
+        <div className="home-content">
+          <h1>
+            Find the book <br/>that
+            you were <br/>meant to read next.
+          </h1>
+          <p>
+            15 questions. 5 matches. <br />
+            Just a quiz that took its time to uderstand you
+          </p>
+          <button onClick={handleFindRead}>Find your next read</button>
+        </div>
+        <div className="home-image">
+          <img src="../images/clipart-books.png" alt="stack of books" />
+        </div>
+      </section>
+      <section className="table-section">
+        <div>
+            <h2>How it works</h2>
+        </div>
+        <table>
+          <thead>
+            <tr className="first-table-header">
+              <th>01</th>
+              <th>02</th>
+              <th>03</th>
+            </tr>
+            <tr className="second-table-header">
+              <th>Take the quiz</th>
+              <th>Get matched</th>
+              <th>Start reading</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Answer 15 thoughtful questions about your personality.</td>
+              <td>Our algorithm finds books that resonate with who you are.</td>
+              <td>
+                Discover your curated list, save favorites, or retake the quiz
+                for fresh picks.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button onClick={handleFindRead}>Take Test</button>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/personality-quiz/Quiz.jsx
@@ -0,0 +1,160 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import './personality-quiz.css';
+
+const Quiz = () => {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [scores, setScores] = useState({
+    Adventurer: 0,
+    Comedic: 0,
+    Romantic: 0,
+    Detective: 0,
+    Horror: 0,
+  });
+  const [isComplete, setIsComplete] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("/mock-data/personalityQuestions.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to load file");
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        setQuestions(jsonData);
+      })
+      .catch((error) => {
+        console.log("Error loading data:", error);
+      });
+  }, []);
+
+  if (questions.length === 0) {
+    return <p>Loading quiz...</p>;
+  } else {
+    const handleUserChoice = (choice) => {
+      if (selectedCategory === "") {
+        setSelectedCategory(choice.category);
+        updateScore(choice);
+
+        if (currentQuestion === questions.length - 1) {
+          setIsComplete(true);
+        }
+      }
+    }; //The object of whatever user chooses get passed into choice
+
+    const handleNextQuestion = () => {
+      if (currentQuestion < questions.length - 1) {
+        setCurrentQuestion((previousQuestion) => {
+          return previousQuestion + 1;
+        });
+        setSelectedCategory("");
+      }
+    };
+
+    const updateScore = (choice) => {
+      if (choice.category === "Adventurer") {
+        setScores((prevScores) => {
+          return {
+            ...prevScores,
+            Adventurer: prevScores.Adventurer + 1,
+          };
+        });
+      } else if (choice.category === "Comedic") {
+        setScores((prevScores) => {
+          return {
+            ...prevScores,
+            Comedic: prevScores.Comedic + 1,
+          };
+        });
+      } else if (choice.category === "Romantic") {
+        setScores((prevScores) => {
+          return {
+            ...prevScores,
+            Romantic: prevScores.Romantic + 1,
+          };
+        });
+      } else if (choice.category === "Detective") {
+        setScores((prevScores) => {
+          return {
+            ...prevScores,
+            Detective: prevScores.Detective + 1,
+          };
+        });
+      } else if (choice.category === "Horror") {
+        setScores((prevScores) => {
+          return {
+            ...prevScores,
+            Horror: prevScores.Horror + 1,
+          };
+        });
+      }
+    };
+
+    const handleGetResults = () => {
+      const result = calculateResults(scores);
+      navigate("/results", { state: { result } });
+    };
+
+    const calculateResults = (scores) => {
+      let highestScore = 0;
+      let winningCategory = "";
+
+      for (let key in scores) {
+        if (scores[key] > highestScore) {
+          highestScore = scores[key];
+          winningCategory = key;
+        }
+      }
+      return winningCategory;
+    };
+
+    return (
+      <main className="quiz-page">
+        <div className="quiz-container">
+          <p className="quiz-progress">
+            Question {currentQuestion + 1} of {questions.length}
+          </p>
+
+          <h2 className="quiz-question">
+            {questions[currentQuestion].question}
+          </h2>
+
+          <div className="quiz-choices">
+            {questions[currentQuestion].choices.map((choice) => {
+              return (
+                <button
+                  key={choice.id}
+                  onClick={() => handleUserChoice(choice)}
+                  disabled={selectedCategory !== ""}
+                >
+                  {choice.text}
+                </button>
+              );
+            })}
+          </div>
+
+          <div className="quiz-navigation">
+            {currentQuestion < questions.length - 1 && (
+              <button 
+                onClick={handleNextQuestion} 
+                disabled={selectedCategory === ""}
+              >
+                Next Question
+              </button>
+            )}
+
+            {isComplete && (
+              <button onClick={handleGetResults}>Get Results</button>
+            )}
+          </div>
+        </div>
+      </main>
+    );
+  }
+};
+
+export default Quiz;
